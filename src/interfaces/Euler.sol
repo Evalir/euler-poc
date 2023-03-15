7a8ea311402a8557d34a3c2b5529a36c55c47ec3
Enrique Ortiz <hi@enriqueortiz.dev>
2023-03-15 13:48:06 -0400

    feat: add missing interfaces

--- src/interfaces/Euler.sol
+++ src/interfaces/Euler.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-interface Liquidation {
+interface IEuler {
     /// @notice Information about a prospective liquidation opportunity
     struct LiquidationOpportunity {
         uint256 repay;
